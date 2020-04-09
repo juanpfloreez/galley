b7c3c59816dc183a10af3963f4305df57e83f0d6
Juan Pablo Flórez Molina <juan@sonartelematics.com>
2020-04-09 00:23:34 -0500

    IU improvements

--- gallery.js
+++ gallery.js
@@ -2,12 +2,14 @@ const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const galleryImgs = document.querySelectorAll(".gallery-img");
 let currentlySelected = 0;
+const indexNumber = document.querySelector(".indexnumber");
 
 prevBtn.addEventListener("click", function(){
 
     galleryImgs[currentlySelected].classList.remove("active");
     currentlySelected = currentlySelected - 1;
-    
+    indexNumber.innerHTML = currentlySelected + 1;
+
     galleryImgs[currentlySelected].classList.add("active");
     nextBtn.disabled = false;
 
@@ -17,9 +19,10 @@ prevBtn.addEventListener("click", function(){
 })
 
 nextBtn.addEventListener("click", function(){
-
+    
     galleryImgs[currentlySelected].classList.remove("active");
     currentlySelected = currentlySelected + 1;
+    indexNumber.innerHTML = currentlySelected + 1;
 
     galleryImgs[currentlySelected].classList.add("active");
     prevBtn.disabled = false;
@@ -28,3 +31,5 @@ nextBtn.addEventListener("click", function(){
         nextBtn.disabled = true;
     }
 })
+
+indexNumber.innerHTML = currentlySelected + 1;
